94795e74d5feedd9fb3f3379dd6d4b85b6519daa
Suveer Chaudhary <suveerchaudhary@Suveers-MacBook-M4-Pro.local>
2026-08-07 03:13:59 -0600

    allow sts:TagSession again

--- bootstrap/oidc/main.tf
+++ bootstrap/oidc/main.tf
@@ -38,7 +38,7 @@ resource "aws_iam_role" "github_actions" {
       Principal = {
         Federated = aws_iam_openid_connect_provider.github.arn
       }
-      Action = "sts:AssumeRoleWithWebIdentity"
+      Action = ["sts:AssumeRoleWithWebIdentity", "sts:TagSession"]
       Condition = {
         StringEquals = {
           "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
